095536f702882c001c5f3dd821a0df7b65dae2c4
jakubkaliszewski <kontakt@jakubkaliszewski.pl>
2020-05-01 13:28:32 +0200

    Reorganizacja struktury. Instalacja pug engine

--- index.mjs
+++ index.mjs
@@ -1,8 +1,15 @@
 import express from 'express';
+import UsosCommunication from "./UsosAuth/usosCommunication.mjs";
+import {usosApiRouter}  from './routes/api/usosAPI.mjs';
 
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+/*App settings*/
+app.set('view engine', 'pug')
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use('/api/usos', usosApiRouter);
+UsosCommunication.getRequestToken();
