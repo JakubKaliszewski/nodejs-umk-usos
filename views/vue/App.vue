431ef5b396e45f5c1d99e76a6c31cc127bdd8472
jakubkaliszewski <kontakt@jakubkaliszewski.pl>
2020-05-02 23:32:11 +0200

    Podgląd wyglądu

--- views/vue/App.vue
+++ views/vue/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <Searchbar/>
-        <Person/>
+        <div id="persons">
+            <Person/>
+            <Person/>
+            <Person/>
+        </div>
     </div>
 </template>
 <script>
     import Searchbar from "./components/Searchbar.vue";
-    import Person from "./components/Hello.vue";
+    import Person from "./components/Person.vue";
     export default {
         components: {Searchbar, Person}
     }
